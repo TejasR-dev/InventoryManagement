a43109cc3ca49d72af58236e154a3e57ebf36acc
tejasr-bit <tejas.r@campusuvce.in>
2026-08-04 20:22:17 +0530

    handle deployement

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql'
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'mysql'
+//   }
+// );
+
+const sequelize = new Sequelize(process.env.MYSQL_PUBLIC_URL, {
+    dialect: "mysql",
+    logging: false,
+});
 
 module.exports = sequelize;
